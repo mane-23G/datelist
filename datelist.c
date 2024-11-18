1e5e71f33b605db3cc587f808730e0ac6f00a062
mane-23G <mane.galstyan87@myhunter.cuny.edu>
2024-11-18 14:35:29 -0500

    fixed error of accepting 0 as valid date modification

--- datelist.c
+++ datelist.c
@@ -167,7 +167,7 @@ int  get_int(char *arg, int flags, int *value, char *msg )
     int res =  get_long(arg, flags, &val, msg );
 
     if ( VALID_NUMBER == res ) {
-        if ( val > INT_MAX  || val < INT_MIN ) {
+        if ( val > INT_MAX  || val < 1 ) {
             sprintf(msg, "%ld is out of range\n", val);
             return OUT_OF_RANGE;
         }
